0a31383f0ef5828e79950511ce7a96d72a10f63d
andressyj24 <andressyj24@yahoo.com>
2014-08-31 22:09:17 -0500

    Se crea el controlador CanjeoMillasController

---
+++ millasweb/src/main/java/com/aerolineasagiles/controller/CanjeoMillasController.java
@@ -0,0 +1,10 @@
+package com.aerolineasagiles.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class CanjeoMillasController {
+
+}
