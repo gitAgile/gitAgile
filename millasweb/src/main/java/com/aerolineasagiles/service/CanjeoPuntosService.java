16dcda88e441feef9a50d7dc18e5674ad05a5125
andressyj24 <andressyj24@yahoo.com>
2014-09-03 22:48:29 -0500

    configuracion unitils

---
+++ millasweb/src/main/java/com/aerolineasagiles/service/CanjeoPuntosService.java
@@ -0,0 +1,13 @@
+package com.aerolineasagiles.service;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class CanjeoPuntosService {
+
+	public Long obtenerPuntosAcumulados(Long codigoCliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
