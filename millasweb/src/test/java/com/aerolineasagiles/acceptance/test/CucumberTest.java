a2be15c09a9ee95d6cb163488f508badb4c0e350
andressyj24 <andressyj24@yahoo.com>
2014-09-12 22:59:00 -0500

    se ignora test de cucumber

--- millasweb/src/test/java/com/aerolineasagiles/acceptance/test/CucumberTest.java
+++ millasweb/src/test/java/com/aerolineasagiles/acceptance/test/CucumberTest.java
@@ -1,9 +1,11 @@
 package com.aerolineasagiles.acceptance.test;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import cucumber.junit.Cucumber;
 
+@Ignore
 @RunWith(Cucumber.class)
 @Cucumber.Options(format = { "pretty", "html:target/cucumber" })
 public class CucumberTest {
